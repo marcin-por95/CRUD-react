02a7de64fa9abcf491b9480c023c221a669ecc64
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-07-04 16:05:32 +0200

    add Validation in PostForm and red style on error message

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import ReactQuill from 'react-quill';
 import DatePicker from 'react-datepicker';
 import 'react-quill/dist/quill.snow.css';
 import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
 
-
-const PostForm = ({data, formHandler, buttonTitle}) => {
+const PostForm = ({ data, formHandler, buttonTitle }) => {
+    const {
+        register,
+        handleSubmit: validate,
+        formState: { errors },
+    } = useForm();
     const [formState, setFormState] = useState(data);
     const formElements = [
-        {name: 'title', title: "Title", type: 'text', placeholder: 'Enter title'},
-        {name: 'author', title: "Author", type: 'text', placeholder: 'Enter author'},
-        {name: 'publishedDate', title: "Published", type: 'text', placeholder: 'Enter date'},
+        { name: 'title', title: "Title", type: 'text', placeholder: 'Enter title' },
+        { name: 'author', title: "Author", type: 'text', placeholder: 'Enter author' },
+        { name: 'publishedDate', title: "Published", type: 'text', placeholder: 'Enter date' },
         {
             name: 'shortDescription',
             title: "Short Description",
@@ -19,13 +24,13 @@ const PostForm = ({data, formHandler, buttonTitle}) => {
             placeholder: 'Leave a comment here',
             as: "textarea"
         },
-        {name: 'content', title: "Main content", type: 'text', placeholder: 'Leave a comment here', as: "textarea"},
+        { name: 'content', title: "Main content", type: 'text', placeholder: 'Leave a comment here', as: "textarea" },
     ];
     const onChangeHandler = (value, name) => {
         if (value === "") {
             setFormState((prevState) => {
                 delete prevState[name];
-                return {...prevState};
+                return { ...prevState };
             });
         } else {
             setFormState((prevState) => ({
@@ -33,6 +38,10 @@ const PostForm = ({data, formHandler, buttonTitle}) => {
                 [name]: value
             }));
         }
+    };
+
+    const errorStyles = {
+        color: 'red',
     };
 
     const getElementByName = (el) => {
@@ -46,22 +55,45 @@ const PostForm = ({data, formHandler, buttonTitle}) => {
             />
         }
         if (el.name === 'publishedDate') {
-            return <DatePicker selected={formState?.[el.name]}
-                               onChange={(value) => onChangeHandler(value, el.name)}
-                               onSelect={(value) => onChangeHandler(value, el.name)}/>
+            return <DatePicker
+                selected={formState?.[el.name]}
+                onChange={(value) => onChangeHandler(value, el.name)}
+                onSelect={(value) => onChangeHandler(value, el.name)}
+            />
         }
-        return <Form.Control
-            type={el.type}
-            placeholder={el.placeholder}
-            as={el.as}
-            onChange={(e) => onChangeHandler(e.target.value, el.name)}
-            defaultValue={data?.[el.name]}
-        />
+        return (
+            <>
+                <Form.Control
+                    {...register(el.name, {
+                        required: true,
+                        minLength: el.name === 'title' || el.name === 'author' ? 3 : undefined,
+                        minLength: el.name === 'shortDescription' ? 20 : undefined,
+                    })}
+                    type={el.type}
+                    placeholder={el.placeholder}
+                    as={el.as}
+                    onChange={(e) => onChangeHandler(e.target.value, el.name)}
+                    defaultValue={data?.[el.name]}
+                />
+                {errors[el.name] && errors[el.name].type === "required" && (
+                    <span style={errorStyles}>This field is required</span> // Dodajemy styl wewnętrzny
+                )}
+                {errors[el.name] && errors[el.name].type === "minLength" && (
+                    <span style={errorStyles}>
+                        {el.name === 'title' || el.name === 'author' ? "Minimum length is 3 characters" : "Minimum length is 20 characters"}
+                    </span> // Dodajemy styl wewnętrzny
+                )}
+            </>
+        );
     }
+
+    const onSubmit = (data) => {
+        formHandler(data);
+    };
 
     return (
         <div className="post-form">
-            <Form>
+            <Form onSubmit={validate(onSubmit)}>
                 {formElements.map((el) => (
                     <Form.Group key={el.name}>
                         <Form.Label>{el.title}</Form.Label>
@@ -69,7 +101,7 @@ const PostForm = ({data, formHandler, buttonTitle}) => {
                     </Form.Group>
                 ))}
                 <Button
-                    onClick={() => formHandler(formState)}
+                    type="submit"
                     disabled={formState != null && Object.values(formState).every((x) => !x)}
                 >
                     {buttonTitle}
